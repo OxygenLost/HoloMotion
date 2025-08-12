83d4661649e523b462eafd0df23b53cb6b9b01d6
maiyue01.chen <maiyue01.chen@horizon.auto>
2025-08-12 10:13:10 +0800

    updated finetune config

--- holomotion/scripts/evaluation/eval_motion_tracking_isaacgym.sh
+++ holomotion/scripts/evaluation/eval_motion_tracking_isaacgym.sh
@@ -3,8 +3,8 @@ source train.env
 export CUDA_VISIBLE_DEVICES="0"
 
 # Configuration
-checkpoint_path="logs/HoloMotion/20250804_205352-train_unitree_g1_21dof_student/model_0.pt" # REQUIRED: Set this to your checkpoint path
-lmdb_path="data/lmdb_datasets/lmdb_g1_21dof_test"
+checkpoint_path="logs/HoloMotion/20250811_232409-train_unitree_g1_23dof_teacher_stage2_robodance100_ft/model_244000.pt"
+lmdb_path="data/lmdb_datasets/lmdb_unitree_G1_23dof_robodance100"
 num_envs=4
 
 ${Train_CONDA_PREFIX}/bin/accelerate launch \
@@ -14,7 +14,7 @@ ${Train_CONDA_PREFIX}/bin/accelerate launch \
     holomotion/src/evaluation/eval_motion_tracking.py \
     use_accelerate=True \
     num_envs=${num_envs} \
-    headless=True \
+    headless=false \
     export_policy=True \
     env.config.termination.terminate_when_motion_far=False \
     +robot.motion.motion_file="${lmdb_path}" \
